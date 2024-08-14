f5668e7c0f25417a8587697dd74e234cf69b5ac6
Ryan Avery <ryanbarryavery@gmail.com>
2024-08-13 18:33:23 -0700

    change port to vercel port

--- src/mlm_form/__init__.py
+++ src/mlm_form/__init__.py
@@ -2,5 +2,5 @@ from .app import app
 import uvicorn
 
 def main() -> int:
-    uvicorn.run("mlm_form:app", host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run("mlm_form:app", host='0.0.0.0', port=5001, reload=True)
     return 0
