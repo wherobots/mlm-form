20ed936afea0d64299fa71bd2651d75a60e9d35e
Ryan Avery <ryanbarryavery@gmail.com>
2024-08-16 13:16:44 -0700

    change port to 3000

--- src/mlm_form/__init__.py
+++ src/mlm_form/__init__.py
@@ -2,5 +2,5 @@ from .app import app
 import uvicorn
 
 def main() -> int:
-    uvicorn.run("mlm_form:app", host='0.0.0.0', port=5001, reload=True)
+    uvicorn.run("mlm_form:app", host='0.0.0.0', port=3000, reload=True)
     return 0
