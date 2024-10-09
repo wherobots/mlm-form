5ae6b74cd2963ddbb52f805d4ffec511f959c263
Ryan Avery <ryan@wherobots.com>
2024-10-09 12:08:38 -0700

    improve description

--- main.py
+++ main.py
@@ -105,14 +105,16 @@ def session_form(session, submitOnLoad=False):
     trigger = "input delay:200ms, load" if submitOnLoad and result else "input delay:200ms"
     session_form = Form(hx_post='/submit', hx_target='#result', hx_trigger=trigger, id="session_form", hx_swap_oob="#session_form", style=form_style)(
                     P(
-                        "The STAC Machine Learning Model (MLM) metadata specification makes it easy to describe the metadata needed to reproduce model inference "
-                        "and enable search and discovery. The MLM makes it easier to share, reuse, and run models "
-                        "on inference providers that support it, such as",
+                        "The ",
+                        A("STAC Machine Learning Model", href="https://github.com/stac-extensions/mlm/blob/main/README.md", target="_blank", rel="noopener noreferrer", cls="border-b-2 border-b-black/30 hover:border-b-black/80"),
+                        "(MLM) metadata specification makes it easy to describe the metadata needed to reproduce model inference and enable search and discovery. The MLM makes it easier to share, reuse, and run models "
+                        "on inference providers that support it, such as ",
                         A("Wherobots", href="https://wherobots.com/wherobotsai-for-raster-inference/", target="_blank", rel="noopener noreferrer", cls="border-b-2 border-b-black/30 hover:border-b-black/80"),
                         ", and open source ml frameworks.\n\n"
-                        "Please complete all required fields below prior to copying or downloading the JSON result. For more information on the specification, refer to the ",
+                        "Please complete all required fields below and in the Asset Form prior to copying or downloading the JSON result. Downloaded JSONs will be stored in your download directory and named based on Model Name field.", 
+                        "For more information on the specification, refer to the ",
                         A("MLM documentation", href="https://github.com/stac-extensions/mlm/blob/main/README.md", target="_blank", rel="noopener noreferrer", cls="border-b-2 border-b-black/30 hover:border-b-black/80"),
-                        "."
+                        ".",
                     ),
                     inputTemplate(label="Model Name", name="model_name", placeholder="A unique identifier for your model", val='', input_type='text'),
                     inputTemplate(label="Architecture", name="architecture", placeholder="A recognizable name for the model architecture", val='', input_type='text'),
